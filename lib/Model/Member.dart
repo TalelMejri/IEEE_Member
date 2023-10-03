e19731b1794704ffa1535affa96e7e3104c79168
TalelMejri <talelmejri8@gmail.com>
2023-10-03 23:43:45 +0100

    Member

---
+++ lib/Model/Member.dart
@@ -0,0 +1,12 @@
+
+
+class Member{
+  String nom;
+  String lastname;
+  String email;
+  bool ispayer;
+  bool present;
+  String chaptername;
+  Member({required this.nom,required this.lastname,required this.email,required this.ispayer,
+  required this.present,required this.chaptername});
+}

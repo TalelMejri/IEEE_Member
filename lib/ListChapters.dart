02539f8d85fb93b304764c636b79a0b966bef29e
TalelMejri <talelmejri8@gmail.com>
2023-10-04 16:25:27 +0100

    countIEE

--- lib/ListChapters.dart
+++ lib/ListChapters.dart
@@ -1,14 +1,44 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:member_ieee/InfoDeveloper.dart';
+import 'package:member_ieee/Model/Member.dart';
 import 'Model/chapters.dart';
 import 'PgesForMembers.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ListChapter extends StatefulWidget {
   @override
   State<ListChapter> createState() => _ListChapter();
 }
-
 class _ListChapter extends State<ListChapter> {
+  
+final storage = new FlutterSecureStorage();
+
+int countIEE=0;
+int payerIEE=0;
+int NotPayerIEEE=0;
+
+void getFromLocalStoreg() async {
+  final usersJson = await storage.read(key: "Members");
+  if (usersJson != null) {
+    final userdecode = jsonDecode(usersJson);
+    List<Member> data = userdecode.map<Member>((user) => Member.fromJson(user)).toList();
+    setState(() {
+        countIEE=data.length;
+        payerIEE=data.where((element) => element.ispayer==true).length;
+        NotPayerIEEE=data.where((element) => element.ispayer==false).length;
+    });
+  }
+}
+
+  @override
+  void initState(){
+    super.initState();
+    getFromLocalStoreg();
+  }
+
+   
   List<cahpters> groupChapters = [
     cahpters(Nom: "CS", ImaggPath: "images/cs.PNG"),
     cahpters(Nom: "WIE", ImaggPath: "images/wie.PNG"),
@@ -24,12 +54,12 @@ class _ListChapter extends State<ListChapter> {
         title: const Center(child: Text("Welcome To IEEE")),
         actions: <Widget>[
            IconButton(onPressed: 
-             (){Navigator.push(context,MaterialPageRoute(builder: (context)=>const InfoDeveloper()));}
+             (){Navigator.push(context,MaterialPageRoute(builder: (context)=> InfoDeveloper(Memebrs:countIEE,NotpayerNumber: NotPayerIEEE,payerNumber: payerIEE,)));}
            , icon: const Icon(Icons.info))
         ],
       ),
       body: Center(
-        child: Padding(padding: EdgeInsets.only(top: 120),child: GridView.builder(
+        child: Padding(padding:const EdgeInsets.only(top: 120),child: GridView.builder(
           itemCount: groupChapters.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
